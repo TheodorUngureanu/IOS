85722903815c57b0b2ef1fa954f971b67ec0e832
Theodor Ungureanu <ungureanu.theo@gmail.com>
2019-07-23 18:16:05 +0300

    Added first app try
    * first commit

---
+++ SoundEfect/SoundEfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundEfect
+//
+//  Created by Theodor Ungureanu on 7/23/19.
+//  Copyright © 2019 Snowlix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
