9fd30ecc93203f4ec29ec0b5acae57e6de5fc26d
Theodor Ungureanu <ungureanu.theo@gmail.com>
2019-10-03 23:28:50 +0300

    Added button image

--- SoundEfect/SoundEfect/ViewController.swift
+++ SoundEfect/SoundEfect/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     @IBOutlet weak var recordingLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     @IBAction func recordAudio(_ sender: Any) {
