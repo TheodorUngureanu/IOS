7693bc94fb6699f3ab877c46a8bf1ffaf074144c
Theodor Ungureanu <ungureanu.theo@gmail.com>
2020-04-23 23:14:56 +0300

    Cleanup

---
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Theodor Ungureanu on 4/23/20.
+//  Copyright © 2020 Snowlix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
